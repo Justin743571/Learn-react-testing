1439361c2c5dcb12aba10d3bfd577df0a31aff70
王旭 <wxu743571@gmail.com>
2024-06-11 20:16:42 +0800

    测试ProductImageGallery组件

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
+
+describe("ProductImageGallery", () => {
+  it("如果给一个空数组,应该不渲染", () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+  it("应该渲染图像列表", () => {
+    const imageUrls = ["asgas", "sfasfa", "svdfsfd"];
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+    const images = screen.getAllByRole("img");
+    expect(images).toHaveLength(3);
+    imageUrls.forEach((url,index) =>{
+        expect(images[index]).toHaveAttribute("src",url)
+    })
+  });
+});
