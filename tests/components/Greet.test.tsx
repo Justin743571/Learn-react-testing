5c8d5405901e85b4b8c1ef23505b0845258f4c0b
王旭 <wxu743571@gmail.com>
2024-06-11 18:48:29 +0800

    简化测试文件导入的模块

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,7 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
+import { render, screen } from '@testing-library/react'
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
 
 describe("group", () => {
   it("如果提供参数name 应该渲染hello和name", () => {
