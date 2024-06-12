7ee5a3fa9a55d3de6157c9d81ecc18e44d8ffd4e
王旭 <wxu743571@gmail.com>
2024-06-12 21:26:26 +0800

    测试异步代码

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import TagList from "../../src/components/TagList";
+
+describe("TagList", () => {
+  it("应呈现Tags", async () => {
+    render(<TagList />);
+
+    // await waitFor(() =>{
+    //     const listItems = screen.getAllByRole("listitem");
+    //     expect(listItems.length).toBeGreaterThan(0);
+    // })
+    
+    const listItems = await screen.findAllByRole("listitem");
+    expect(listItems.length).toBeGreaterThan(0);
+
+  });
+});
