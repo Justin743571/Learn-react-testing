fb3b82bb179af4b2036709030504254afc6994ba
王旭 <wxu743571@gmail.com>
2024-06-13 20:18:13 +0800

    模拟数据

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -3,17 +3,27 @@ import ProductDetail from "../../src/components/ProductDetail";
 import { products } from "../mocks/data";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
+import { db } from "../mocks/db";
 
 describe("ProductDetail", () => {
+  let productId: number;
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id;
+  });
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } });
+  });
   it("应呈现具体产品名和价格", async () => {
-    render(<ProductDetail productId={1} />);
+    const product = db.product.findFirst({where:{id:{equals:productId}}})
+    render(<ProductDetail productId={productId} />);
 
-    expect(await screen.findByText(new RegExp(products[0].name)));
-    expect(await screen.findByText(new RegExp(products[0].price.toString())));
+    expect(await screen.findByText(new RegExp(product!.name)));
+    expect(await screen.findByText(new RegExp(product!.price.toString())));
   });
   it("如果产品没有找到 应该呈现一段文字", async () => {
     server.use(http.get("products/1", () => HttpResponse.json(null)));
-    render(<ProductDetail productId={1} />);
+    render(<ProductDetail productId={productId} />);
 
     expect(await screen.findByText(/not found/i)).toBeInTheDocument();
   });
