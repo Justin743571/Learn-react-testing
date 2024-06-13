d10c76346c347196d749f3fc522ac6360861e68b
王旭 <wxu743571@gmail.com>
2024-06-13 21:20:18 +0800

    重构：使用ReactQuery

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -1,8 +1,13 @@
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import ProductList from "../../src/components/ProductList";
 import { server } from "../mocks/server";
-import { http, HttpResponse,delay } from "msw";
+import { http, HttpResponse, delay } from "msw";
 import { db } from "../mocks/db";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 describe("ProductList", () => {
   const productIds: number[] = [];
@@ -16,8 +21,25 @@ describe("ProductList", () => {
     db.product.deleteMany({ where: { id: { in: productIds } } });
   });
 
+  const renderProductList = () => {
+    const client = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+    render(
+      <>
+        <QueryClientProvider client={client}>
+          <ProductList />
+        </QueryClientProvider>
+      </>
+    );
+  };
+
   it("应呈现产品列表", async () => {
-    render(<ProductList />);
+    renderProductList()
     const items = await screen.findAllByRole("listitem");
     expect(items.length).toBeGreaterThan(0);
   });
@@ -25,39 +47,42 @@ describe("ProductList", () => {
   it("如果没有产品 应呈现no products", async () => {
     server.use(http.get("/products", () => HttpResponse.json([])));
 
-    render(<ProductList />);
+    renderProductList()
     const message = await screen.findByText(/no products/i);
     expect(message).toBeInTheDocument();
   });
-  
+
   it("出现错误时，应呈现错误信息", async () => {
     server.use(http.get("/products", () => HttpResponse.error()));
 
-    render(<ProductList />);
+    renderProductList()
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 
-  it('如果在获取数据时 应呈现loading文本', async() => {
-    server.use(http.get("/products", async() => {
-      await delay()
-      return HttpResponse.json([])}));
+  it("如果在获取数据时 应呈现loading文本", async () => {
+    server.use(
+      http.get("/products", async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
 
-      render(<ProductList />);
+    renderProductList()
 
-  expect(await screen.findByText(/loading/i)).toBeInTheDocument();
-  })
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
+  });
 
-  it('在获取完数据 应呈现去除loading文本', async() => {
-    render(<ProductList/>)
-    
-    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
-  })
+  it("在获取完数据 应呈现去除loading文本", async () => {
+    renderProductList()
 
-  it('在获取完数据出现错误 应呈现去除loading文本', async() => {
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  });
+
+  it("在获取完数据出现错误 应呈现去除loading文本", async () => {
     server.use(http.get("/products", () => HttpResponse.error()));
-    
-    render(<ProductList/>)
 
-    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
-  })
+    renderProductList()
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  });
 });
