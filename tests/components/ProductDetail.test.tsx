82ecf85fff6963916384225a409887848b5d41c2
王旭 <wxu743571@gmail.com>
2024-06-13 20:19:55 +0800

    修复问题，关于ProductDetail.test.tsx

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
-import { products } from "../mocks/data";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
 import { db } from "../mocks/db";
@@ -22,7 +21,7 @@ describe("ProductDetail", () => {
     expect(await screen.findByText(new RegExp(product!.price.toString())));
   });
   it("如果产品没有找到 应该呈现一段文字", async () => {
-    server.use(http.get("products/1", () => HttpResponse.json(null)));
+    server.use(http.get("products/"+productId, () => HttpResponse.json(null)));
     render(<ProductDetail productId={productId} />);
 
     expect(await screen.findByText(/not found/i)).toBeInTheDocument();
