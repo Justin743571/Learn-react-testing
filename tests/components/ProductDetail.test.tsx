a5ddd0ab137f3355c110162c7db27287b7d03d01
王旭 <wxu743571@gmail.com>
2024-06-13 20:59:13 +0800

    测试loading文本

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
 import { server } from "../mocks/server";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
 import { db } from "../mocks/db";
 
 describe("ProductDetail", () => {
@@ -38,5 +38,26 @@ describe("ProductDetail", () => {
     render(<ProductDetail productId={productId} />);
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  })
+
+  it('如果数据正在加载 应呈现加载文本', async() => {
+    server.use(http.get("products/"+productId, async() => {
+      await delay()
+      return HttpResponse.json(null)}));
+
+      render(<ProductDetail productId={productId} />);
+    
+      expect(await screen.findByText(/loading/i))
+  })
+  it('如果数据加载完成 loading文本应去除', async() => {
+    render(<ProductDetail productId={productId} />);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+  })
+  it('如果数据加载错误 loading文本应去除', async() => {
+    server.use(http.get("products/"+productId, () => HttpResponse.error()));
+    render(<ProductDetail productId={productId} />);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
   })
 });
