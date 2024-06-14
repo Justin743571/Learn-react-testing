8ddfff77c11ec7d0db453a23121df2b60c85a101
王旭 <wxu743571@gmail.com>
2024-06-14 15:03:51 +0800

    重构测试

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -3,14 +3,13 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import { server } from "../mocks/server";
-import { delay, http, HttpResponse } from "msw";
 import BrowseProducts from "../../src/pages/BrowseProductsPage";
 import { Theme } from "@radix-ui/themes";
 import userEvent from "@testing-library/user-event";
 import { db } from "../mocks/db";
 import { Category, Product } from "../../src/entities";
 import { CartProvider } from "../../src/providers/CartProvider";
+import { simulateDelay, simulateError } from "../utils";
 
 describe("BrowseProductsPage", () => {
   const categories: Category[] = [];
@@ -36,90 +35,77 @@ describe("BrowseProductsPage", () => {
         </Theme>
       </CartProvider>
     );
+    return {
+      getProductsSkeleton: () =>
+        screen.queryByRole("progressbar", { name: /products/i }),
+      getCategoriesSkeleton: () =>
+        screen.queryByRole("progressbar", { name: /categories/i }),
+      getCategoriesComboBox: () => screen.queryByRole("combobox"),
+    };
   };
 
   it("如果正在获取Categories数据 应呈现加载骨架", () => {
-    server.use(
-      http.get("/categories", async () => {
-        await delay();
-        return HttpResponse.json([]);
-      })
-    );
+    simulateDelay("/categories");
 
-    renderBrowseProducts();
+    const { getCategoriesSkeleton } = renderBrowseProducts();
 
-    expect(
-      screen.getByRole("progressbar", { name: /categories/i })
-    ).toBeInTheDocument();
+    expect(getCategoriesSkeleton()).toBeInTheDocument();
   });
 
   it("如果获取完Categories数据 应呈现隐藏加载骨架", async () => {
-    renderBrowseProducts();
+    const { getCategoriesSkeleton } = renderBrowseProducts();
 
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("progressbar", { name: /categories/i })
-    );
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
   });
 
   it("如果正在获取Products数据 应呈现加载骨架", () => {
-    server.use(
-      http.get("/products", async () => {
-        await delay();
-        return HttpResponse.json([]);
-      })
-    );
+    simulateDelay("/products");
 
-    renderBrowseProducts();
+    const { getProductsSkeleton } = renderBrowseProducts();
 
-    expect(
-      screen.getByRole("progressbar", { name: /products/i })
-    ).toBeInTheDocument();
+    expect(getProductsSkeleton()).toBeInTheDocument();
   });
 
   it("如果获取完Products数据 应呈现隐藏加载骨架", async () => {
-    renderBrowseProducts();
+    const { getProductsSkeleton } = renderBrowseProducts();
 
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("progressbar", { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getProductsSkeleton);
   });
 
   it("如果在获取categories数据失败 不应呈现categories的东西", async () => {
-    server.use(http.get("/categories", () => HttpResponse.error()));
+    simulateError("/categories");
 
-    renderBrowseProducts();
+    const { getCategoriesSkeleton, getCategoriesComboBox } =
+      renderBrowseProducts();
 
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("progressbar", { name: /categories/i })
-    );
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
 
     expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
-    expect(
-      screen.queryByRole("combobox", { name: /category/i })
-    ).not.toBeInTheDocument();
+    expect(getCategoriesComboBox()).not.toBeInTheDocument();
   });
 
   it("如果在获取product数据失败 不应呈现product的东西", async () => {
-    server.use(http.get("/products", () => HttpResponse.error()));
+    simulateError("/products");
 
-    renderBrowseProducts();
+    const { getProductsSkeleton } = renderBrowseProducts();
 
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("progressbar", { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getProductsSkeleton);
 
     expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
   });
 
   it("应渲染categories", async () => {
-    renderBrowseProducts();
-    const combobox = await screen.findByRole("combobox");
+    const { getCategoriesSkeleton, getCategoriesComboBox } =
+      renderBrowseProducts();
 
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
+
+    const combobox = getCategoriesComboBox();
     expect(combobox).toBeInTheDocument();
     expect(combobox).toHaveTextContent(/category/i);
 
     const user = userEvent.setup();
-    await user.click(combobox);
+    await user.click(combobox!);
 
     expect(screen.getByRole("option", { name: /all/i })).toBeInTheDocument();
     categories.forEach((category) => {
@@ -130,11 +116,9 @@ describe("BrowseProductsPage", () => {
   });
 
   it("应渲染products", async () => {
-    renderBrowseProducts();
+    const { getProductsSkeleton } = renderBrowseProducts();
 
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("progressbar", { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getProductsSkeleton);
 
     products.forEach((product) => {
       expect(screen.getByText(product.name)).toBeInTheDocument();
