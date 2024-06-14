8a04e957ab3540c63ec2c7b0e748e617b4acbc4e
王旭 <wxu743571@gmail.com>
2024-06-14 14:29:11 +0800

    测试数据渲染

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { factory, primaryKey } from "@mswjs/data";
-import {faker} from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
-
-export const db =factory({
-    product:{
-        id: primaryKey(faker.number.int),
-        name:faker.commerce.productName,
-        price:() => faker.number.int({min:1,max:100}),
-    }
-})
+export const db = factory({
+  product: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.productName,
+    price: () => faker.number.int({ min: 1, max: 100 }),
+    categoryId:faker.number.int,
+  },
+  category: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.department,
+  },
+});
--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -7,13 +7,34 @@ import { server } from "../mocks/server";
 import { delay, http, HttpResponse } from "msw";
 import BrowseProducts from "../../src/pages/BrowseProductsPage";
 import { Theme } from "@radix-ui/themes";
+import userEvent from "@testing-library/user-event";
+import { db } from "../mocks/db";
+import { Category, Product } from "../../src/entities";
+import { CartProvider } from "../../src/providers/CartProvider";
 
 describe("BrowseProductsPage", () => {
+  const categories: Category[] = [];
+  const products: Product[] = [];
+  beforeAll(() => {
+    [1, 2].forEach((item) => {
+      categories.push(db.category.create({ name: "Category" + item }));
+      products.push(db.product.create());
+    });
+  });
+  afterAll(() => {
+    const categoriesId = categories.map((c) => c.id);
+    db.category.deleteMany({ where: { id: { in: categoriesId } } });
+
+    const productId = products.map((p) => p.id);
+    db.product.deleteMany({ where: { id: { in: productId } } });
+  });
   const renderBrowseProducts = () => {
     render(
-      <Theme>
-        <BrowseProducts />
-      </Theme>
+      <CartProvider>
+        <Theme>
+          <BrowseProducts />
+        </Theme>
+      </CartProvider>
     );
   };
 
@@ -88,5 +109,35 @@ describe("BrowseProductsPage", () => {
     );
 
     expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+  });
+
+  it("应渲染categories", async () => {
+    renderBrowseProducts();
+    const combobox = await screen.findByRole("combobox");
+
+    expect(combobox).toBeInTheDocument();
+    expect(combobox).toHaveTextContent(/category/i);
+
+    const user = userEvent.setup();
+    await user.click(combobox);
+
+    expect(screen.getByRole("option", { name: /all/i })).toBeInTheDocument();
+    categories.forEach((category) => {
+      expect(
+        screen.getByRole("option", { name: category.name })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("应渲染products", async () => {
+    renderBrowseProducts();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole("progressbar", { name: /products/i })
+    );
+
+    products.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    });
   });
 });
