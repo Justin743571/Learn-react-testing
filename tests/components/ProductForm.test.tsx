c4a8f5992671e0769fe268d391d77b520aa680c0
王旭 <wxu743571@gmail.com>
2024-06-16 20:13:22 +0800

    测试验证规则

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -4,6 +4,7 @@ import { Category, Product } from "../../src/entities";
 import AllProviders from "../AllProviders";
 import { db } from "../mocks/db";
 import userEvent from "@testing-library/user-event";
+import { number } from "zod";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -80,6 +81,49 @@ describe("ProductForm", () => {
       const user = userEvent.setup();
       if (name !== undefined) await user.type(form.nameInput, name);
       await user.type(form.priceInput, "10");
+      await user.click(form.categoryInput);
+      const options = screen.getAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.getByRole("alert");
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
+
+  it.each([
+    { scenario: "missing", errorMessage: /required/i },
+    {
+      scenario: "not a number",
+      price: "ad",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "nagative",
+      price: -1,
+      errorMessage: /required/i,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitforFormToLoad } = renderComponent();
+
+      const form = await waitforFormToLoad();
+      const user = userEvent.setup();
+      await user.type(form.nameInput, "milk");
+      if (price !== undefined)
+        await user.type(form.nameInput, price.toString());
       await user.click(form.categoryInput);
       const options = screen.getAllByRole("option");
       await user.click(options[0]);
