1dc9e69e60fd0ff868972ca0f98cfb3c43af64da
王旭 <wxu743571@gmail.com>
2024-06-16 20:45:00 +0800

    提取用于填写表格的函数

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ProductForm from "../../src/components/ProductForm";
 import { Category, Product } from "../../src/entities";
 import AllProviders from "../AllProviders";
 import { db } from "../mocks/db";
-import userEvent from "@testing-library/user-event";
-import { number } from "zod";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -21,13 +23,52 @@ describe("ProductForm", () => {
     });
 
     return {
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveTextContent(errorMessage);
+      },
       waitforFormToLoad: async () => {
         await screen.findByRole("form");
+
+        const nameInput = screen.getByPlaceholderText(/name/i);
+        const priceInput = screen.getByPlaceholderText(/price/i);
+        const categoryInput = screen.getByRole("combobox", {
+          name: /category/i,
+        });
+        const submitButton = screen.getByRole("button");
+
+        type FormData = {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          [K in keyof Product]: any;
+        };
+
+        const validData: FormData = {
+          id: 1,
+          name: "a",
+          price: 1,
+          categoryId: 1,
+        };
+
+        const fill = async (product: FormData) => {
+          const user = userEvent.setup();
+          if (product.name !== undefined)
+            await user.type(nameInput, product.name);
+          if (product.price !== undefined)
+            await user.type(nameInput, product.price.toString());
+          await user.click(categoryInput);
+          const options = screen.getAllByRole("option");
+          await user.click(options[0]);
+          await user.click(submitButton);
+        };
+
         return {
-          nameInput: screen.getByPlaceholderText(/name/i),
-          priceInput: screen.getByPlaceholderText(/price/i),
-          categoryInput: screen.getByRole("combobox", { name: /category/i }),
-          submitButton: screen.getByRole("button"),
+          nameInput,
+          priceInput,
+          categoryInput,
+          submitButton,
+          fill,
+          validData,
         };
       },
     };
@@ -65,6 +106,7 @@ describe("ProductForm", () => {
     const { nameInput } = await waitforFormToLoad();
     expect(nameInput).toHaveFocus();
   });
+
   it.each([
     { scenario: "missing", errorMessage: /required/i },
     {
@@ -75,20 +117,13 @@ describe("ProductForm", () => {
   ])(
     "should display an error if name is $scenario",
     async ({ name, errorMessage }) => {
-      const { waitforFormToLoad } = renderComponent();
+      const { waitforFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
 
       const form = await waitforFormToLoad();
-      const user = userEvent.setup();
-      if (name !== undefined) await user.type(form.nameInput, name);
-      await user.type(form.priceInput, "10");
-      await user.click(form.categoryInput);
-      const options = screen.getAllByRole("option");
-      await user.click(options[0]);
-      await user.click(form.submitButton);
+      await form.fill({ ...form.validData, name });
 
-      const error = screen.getByRole("alert");
-      expect(error).toBeInTheDocument();
-      expect(error).toHaveTextContent(errorMessage);
+      expectErrorToBeInTheDocument(errorMessage);
     }
   );
 
@@ -117,21 +152,13 @@ describe("ProductForm", () => {
   ])(
     "should display an error if price is $scenario",
     async ({ price, errorMessage }) => {
-      const { waitforFormToLoad } = renderComponent();
+      const { waitforFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
 
       const form = await waitforFormToLoad();
-      const user = userEvent.setup();
-      await user.type(form.nameInput, "milk");
-      if (price !== undefined)
-        await user.type(form.nameInput, price.toString());
-      await user.click(form.categoryInput);
-      const options = screen.getAllByRole("option");
-      await user.click(options[0]);
-      await user.click(form.submitButton);
+      await form.fill({ ...form.validData, price });
 
-      const error = screen.getByRole("alert");
-      expect(error).toBeInTheDocument();
-      expect(error).toHaveTextContent(errorMessage);
+      expectErrorToBeInTheDocument(errorMessage);
     }
   );
 });
